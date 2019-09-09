3ce1e3eaaed1ef732d0ef0624a3688dcc52f1c26
yuta4j1 <kascado.ys10@gmail.com>
2019-09-09 23:57:00 +0900

    feat: repository list pagenation

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,19 +1,16 @@
 import * as React from "react"
 import RepositoryList from "./RepositoryList"
+import PageNavi from "./PageNavi"
 import { RepositoryData } from "../interfaces"
 import styled from "styled-components"
 import { fetchRepositoryList } from "../api"
-
-type State = {
-  repositories: RepositoryData[]
-}
 
 const DashboardOutline = styled.div`
   width: 90%;
   margin: 8px;
   padding: 8px;
   display: flex;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   font-weight: light;
 `
 const Left = styled.div`
@@ -25,28 +22,103 @@ const Center = styled.div`
   width: 65%;
   padding: 8px;
 `
+const PageNaviWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 12px 8px;
+`
 
 const Right = styled.div`
   width: 15%;
   padding: 8px;
 `
 
-const Dashboard: React.FC<{}> = () => {
-  const initState: State = { repositories: [] }
+type Props = {
+  repositories: RepositoryData[]
+  storeRepositories: (data: RepositoryData[]) => void
+}
+
+type Pagenation = {
+  pageNo: number
+  perPage: number
+}
+
+const initPagenation = {
+  pageNo: 1,
+  perPage: 10,
+  allPageNum: 0
+}
+
+const Dashboard: React.FC<Props> = props => {
+  const initState: RepositoryData[] = []
   const [repos, setRepos] = React.useState(initState)
+  const [pagenation, setPagenation] = React.useState(initPagenation)
+
+  const computeAllPageNum = (repoLength: number, perPage: number): number => {
+    const split = repoLength / perPage
+    const syou = repoLength % perPage
+    if (syou > 0) {
+      return split + 1
+    }
+    return split
+  }
+
+  const setNowPage = (now: number): void => {
+    setPagenation(
+      Object.assign({}, pagenation, {
+        pageNo: now
+      })
+    )
+  }
 
   React.useEffect(() => {
     ;(async () => {
       const repos: RepositoryData[] = await fetchRepositoryList("/list")
       console.log(repos)
-      setRepos({ repositories: repos })
+      props.storeRepositories(repos)
+      setRepos(repos)
+      setPagenation(
+        Object.assign({}, pagenation, {
+          allPageNum: computeAllPageNum(repos.length, pagenation.perPage)
+        })
+      )
     })()
   }, [])
+
+  const paging = (repos: RepositoryData[], p: Pagenation): RepositoryData[] => {
+    const highLimit = (p.pageNo + 1) * p.perPage
+    const lowLimit = p.pageNo * p.perPage
+    if (repos.length > lowLimit && repos.length < highLimit) {
+      // 最後の余り要素のみ表示
+      return repos.filter((data: RepositoryData, i: number) => i > lowLimit - 1)
+    } else if (repos.length > highLimit) {
+      return repos.filter(
+        (data: RepositoryData, i: number) =>
+          i >= lowLimit - 1 && i <= highLimit - 1
+      )
+    } else if (repos.length < highLimit) {
+      const lastPage = repos.length / p.perPage
+      const lastPageFirstIdx = lastPage * p.perPage - 1
+      return repos.filter(
+        (data: RepositoryData, i: number) => i >= lastPageFirstIdx
+      )
+    } else {
+      // 先頭要素のみ取り出す
+      return repos.slice(0, p.perPage - 1)
+    }
+  }
   return (
     <DashboardOutline>
       <Left></Left>
       <Center>
-        <RepositoryList repositoryDatas={repos.repositories} />
+        <RepositoryList repositoryDatas={paging(repos, pagenation)} />
+        <PageNaviWrapper>
+          <PageNavi
+            pageNum={pagenation.allPageNum}
+            nowPage={1}
+            setNowPage={setNowPage}
+          />
+        </PageNaviWrapper>
       </Center>
       <Right></Right>
     </DashboardOutline>
---
+++ components/PageNavi.tsx
@@ -0,0 +1,65 @@
+import * as React from "react"
+import styled from "styled-components"
+
+type Props = {
+  pageNum: number
+  nowPage: number
+  setNowPage: (num: number) => void
+}
+
+const PageNaviArea = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50%;
+  height: auto;
+  padding: 8px;
+`
+
+const PageNo = styled.div`
+  border-radius: 50%;
+  width: 36px;
+  height: 14px;
+  padding: 16px;
+  margin: 16px;
+  color: #42a5f5;
+  font-size: 110%;
+  text-align: center;
+  cursor: pointer;
+  transition-property: all;
+  transition: 0.1s linear;
+  &:hover {
+    background-color: #e1f5fe;
+    color: #81d4fa;
+  }
+`
+
+const numToSerialArray = (num: number): string[] => {
+  return [...Array(num)].map((v, i) => String(i + 1))
+}
+
+const PageNavi: React.FC<Props> = props => {
+  const handlePageNoClicked = (e: React.MouseEvent<HTMLDivElement>) => {
+    const pageNo = e.currentTarget.innerText
+    props.setNowPage(Number(pageNo))
+  }
+  const MAX_PAGE = 5
+  let pageNoArray: string[] = []
+  if (props.pageNum > MAX_PAGE) {
+    pageNoArray = numToSerialArray(4)
+    pageNoArray.push(">>")
+  } else {
+    pageNoArray = numToSerialArray(props.pageNum)
+  }
+
+  return (
+    <PageNaviArea>
+      {pageNoArray.map((v, i) => (
+        <PageNo key={i} onClick={e => handlePageNoClicked(e)}>
+          {v}
+        </PageNo>
+      ))}
+    </PageNaviArea>
+  )
+}
+
+export default PageNavi
