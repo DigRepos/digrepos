c4bb627ab9b1292c2a60d6c52054af4eb4b7b318
yuta4j1 <kascado.ys10@gmail.com>
2019-09-22 01:15:43 +0900

    keyframes declaration

---
+++ interfaces/styled-theme.ts
@@ -0,0 +1,11 @@
+import * as styledComponents from 'styled-components'
+
+const { 
+    default: styled,
+    keyframes
+} = styledComponents as styledComponents.ThemedStyledComponentsModule<
+  any
+>
+
+export default styled
+export { keyframes }
