36f4e26ce576afb1554ca9bdc58d428ff8b26707
yuta4j1 <kascado.ys10@gmail.com>
2019-09-25 22:45:24 +0900

    feat: redux devtools

--- containers/store.ts
+++ containers/store.ts
@@ -1,9 +1,16 @@
-import { createStore, combineReducers } from 'redux'
-import { dashboardReposReducer } from './Dashboard/reducer'
-import { setFilterReducer } from './SearchFilter/reducer'
+import { createStore, combineReducers, applyMiddleware } from "redux"
+import { dashboardReposReducer } from "./Dashboard/reducer"
+import { setFilterReducer } from "./SearchFilter/reducer"
+import { composeWithDevTools } from "redux-devtools-extension"
 
-export const rootReducer = combineReducers({ dashboardReposReducer, setFilterReducer })
+export const rootReducer = combineReducers({
+  dashboardReposReducer,
+  setFilterReducer
+})
 
 export type AppState = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer)
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware())
+)
