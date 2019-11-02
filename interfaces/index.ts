f89c6a9c9efb6d5a075cac6d5f948d567a836a85
yuta4j1 <kascado.ys10@gmail.com>
2019-11-03 00:00:48 +0900

    feat: PageNaviState

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,22 +1,21 @@
-
 export type Owner = {
-  name: string,
+  name: string
   avatarUrl: string
 }
 
 export type RepositoryData = {
-  id: string,
-  url: string,
-  avatarUrl: string,
-  fullName: string,
-  description: string,
-  homepage: string,
-  owner: Owner,
-  star: number,
-  forksCount: number,
-  watchersCount: number,
-  topics: string[],
-  language: string,
+  id: string
+  url: string
+  avatarUrl: string
+  fullName: string
+  description: string
+  homepage: string
+  owner: Owner
+  star: number
+  forksCount: number
+  watchersCount: number
+  topics: string[]
+  language: string
   updatedAt: string
 }
 
@@ -25,28 +24,33 @@ export type ModalOverlayStyle = {
 }
 
 export type ModalStyle = {
-  height: string,
-  width: string,
+  height: string
+  width: string
   backgroundColorHex: string
 }
 
 export type SearchFilterModel = {
-  keywords: string[],
+  keywords: string[]
   star: {
-    low: string,
+    low: string
     high: string
-  },
-  language: string,
+  }
+  language: string
   license: string
 }
 
-export type TabKey = 'filter' | 'sort'
+export type TabKey = "filter" | "sort"
 
 // 配列のインデックスと要素を紐づけるため、idxは数値型
 export type DraggableItem = {
-  idx: number,
-  key: string,
+  idx: number
+  key: string
   expr: SortType
 }
 
-export type SortType = 'Star' | 'Fork' | 'Watch' | 'Date'
+export type SortType = "Star" | "Fork" | "Watch" | "Date"
+
+export type PageNaviState = {
+  currentPageNo: number
+  allPageNum: number
+}
