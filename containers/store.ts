3a7644c15259b70524c4792ea9a39e0b94178a7f
yuta4j1 <kascado.ys10@gmail.com>
2019-11-05 22:07:33 +0900

    remove PageNavi container components

--- containers/store.ts
+++ containers/store.ts
@@ -2,14 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import { dashboardReposReducer } from "./Dashboard/reducer"
 import { setFilterReducer } from "./SearchFilter/reducer"
 import { sortReducer } from './Sort/reducer'
-import { pageNaviReducer } from './PageNavi/reducer'
 import { composeWithDevTools } from "redux-devtools-extension"
 
 export const rootReducer = combineReducers({
   dashboardReposReducer,
   setFilterReducer,
   sortReducer,
-  pageNaviReducer,
 })
 
 export type AppState = ReturnType<typeof rootReducer>
