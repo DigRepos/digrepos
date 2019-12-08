563c92af57b6caaa3d2eacae5916561fc7409ea0
yuta4j1 <kascado.ys10@gmail.com>
2019-12-08 12:33:52 +0900

    prettier

--- containers/Sort/reducer.ts
+++ containers/Sort/reducer.ts
@@ -1,16 +1,16 @@
 import { UPDATE_SORT_ORDER } from "../actionTypes"
 import { SortType } from "../../interfaces"
 
-const initialState: SortType[] = ['Star', 'Fork', 'Watch', 'Date']
+const initialState: SortType[] = ["Star", "Fork", "Watch", "Date"]
 
 export function sortReducer(
   state: SortType[] = initialState,
   action: { type: string; payload: SortType[] }
 ): SortType[] {
-    switch (action.type) {
-        case UPDATE_SORT_ORDER:
-            return action.payload
-        default:
-            return state
-    }
+  switch (action.type) {
+    case UPDATE_SORT_ORDER:
+      return action.payload
+    default:
+      return state
+  }
 }
