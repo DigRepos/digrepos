01970acc0e092e58306149d50ecdf232344931bc
yuta4j1 <kascado.ys10@gmail.com>
2020-01-11 18:14:17 +0900

    Apollo provider

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,18 @@ import Dashboard from "../containers/Dashboard"
 import { NextPage } from "next"
 import { Provider } from "react-redux"
 import { store } from "../containers/store"
+import { ApolloProvider } from "@apollo/react-hooks"
+import client from "../shared/gql-client"
 
 const IndexPage: NextPage = () => {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Dashboard />
-      </Layout>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <Layout>
+          <Dashboard />
+        </Layout>
+      </Provider>
+    </ApolloProvider>
   )
 }
 
