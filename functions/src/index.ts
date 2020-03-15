c974afdfcb890f537fb0c8472917ad9c7ff4ed86
yuta4j1 <kascado.ys10@gmail.com>
2020-03-15 15:39:16 +0900

    feat: CDN cache

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,14 @@
 import * as functions from "firebase-functions"
 import next from "next"
-// import Cors from "micro-cors"
 
 const app = next({
   dev: process.env.NODE_ENV !== "production",
   conf: { distDir: ".next" }
 })
-// const cors = Cors({
-//   allowMethods: ["GET", "HEAD", "OPTIONS", "POST"]
-// })
 const handle = app.getRequestHandler()
 
 export const nextApp = functions.https.onRequest((req, res) => {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
   console.log("page url ", req.originalUrl)
   return app.prepare().then(() => handle(req, res))
 })
