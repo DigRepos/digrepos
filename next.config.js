970ba8fcc75843eab86437b4d71d7af945d55703
yuta4j1 <kascado.ys10@gmail.com>
2020-03-15 11:28:32 +0900

    feat: env setting

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const webpack = require("webpack")
+require("dotenv").config()
+
+function webpackConfig(config) {
+  config.plugins = config.plugins || []
+  config.plugins = [
+    ...config.plugins,
+    new webpack.EnvironmentPlugin({
+      FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+      FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID)
+    })
+  ]
+  return config
+}
+
 module.exports = {
-  target: "server"
+  target: "server",
+  webpack: webpackConfig
 }
